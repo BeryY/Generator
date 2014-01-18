ee28a33a17aaf4eb104d5db9fc2b4a69751cc0cc
Pawel_Sykala <pawelsykala3@wp.pl>
2014-01-18 13:22:08 +0100

    Koniec zadania

--- utils.h
+++ utils.h
@@ -1,5 +1,19 @@
 #include<stdlib.h>
 #include <time.h>
+int* generator()
+{	int tab[10];
+	
+	int	wylosowana_liczba;
+	srand( time( NULL ) );
+	int i;
+	for( i=0;i<10;i++)
+	{
+		 wylosowana_liczba =( rand() % 100 ) + 0;
+		tab[i]=wylosowana_liczba;
+		
+	}
+	return tab;
+}
 void wyswietlanie(int *tab)
 {
 	int i;
